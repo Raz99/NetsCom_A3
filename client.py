42a4c59d15ee3682c75c36b69584fc32ffea5e71
ronench20 <ronencher@gmail.com>
2025-01-02 18:17:41 +0200

    commit

--- client.py
+++ client.py
@@ -15,20 +15,28 @@ def send_message(client_socket, message, maximum_msg_size, window_size):
     num_of_messages = math.ceil(message_size / maximum_msg_size)
     i = 0  # Current message index
 
-        # Sends the initial window of messages
-        while i < num_of_messages and i < window_size:
-            start = i * maximum_msg_size
-            end = min(start + maximum_msg_size, message_size)
-            content = message_bytes[start:end]
-            sequence_number = f"{i}" # Adds sequence number
-            while len(sequence_number) < HEADER_SIZE:
-                sequence_number = " " + sequence_number
-            sequence_number = sequence_number.encode('utf-8')
-            package = sequence_number + content
-            client_socket.send(package)  # Sends the package
-            print(f"M{i} has been sent to server (status: {i + 1}/{num_of_messages}):")
-            print(f"Content: \"{content.decode('utf-8')}\"")
-            i += 1
+    # Sends the initial window of messages
+    while i < num_of_messages and i < window_size:
+        start = i * maximum_msg_size
+        end = min(start + maximum_msg_size, message_size)
+        content = message_bytes[start:end]
+        sequence_number = f"{i}" # Adds sequence number
+        while len(sequence_number) < HEADER_SIZE:
+            sequence_number = " " + sequence_number
+        sequence_number = sequence_number.encode('utf-8')
+        package = sequence_number + content
+        client_socket.send(package)  # Sends the package
+        print(f"M{i} has been sent to server (status: {i + 1}/{num_of_messages}):")
+        print(f"Content: \"{content.decode('utf-8')}\"")
+        i += 1
+
+    while True:
+        ack_message = client_socket.recv(1024).decode('utf-8')
+        ack = strip_ack(ack_message)
+        print(f"ACK: {ack} has been received")
+
+        if i >= num_of_messages and i == ack + 1:
+            break
 
         # Sends another package if ACK arrived
         start = i
